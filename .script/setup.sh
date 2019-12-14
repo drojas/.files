5dd93365b0799a32e92d186e5fb4f711ff44b4a5
David Rojas <david@azlo.com>
2019-12-13 22:57:12 -0800

    amend

--- .script/setup.sh
+++ .script/setup.sh
@@ -1,8 +1,8 @@
 # setup
 # source - http://news.ycombinator.com/item?id=11070797
 
-git clone --bare git@github.com:tadyshev/.unix.git $HOME/.unix
-alias dot='git --git-dir=$HOME/.unix/ --work-tree=$HOME'
+git clone --bare git@github.com:drojas/.files.git $HOME/.files
+alias dot='git --git-dir=$HOME/.files/ --work-tree=$HOME'
 
 dot checkout
 dot config status.showUntrackedFiles no
